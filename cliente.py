539807a96bfce492b5bd4b549da56247de767a43
Paulo Calanca <sir.calanca@gmail.com>
2021-06-01 18:28:21 -0300

    base-do-Banco

---
+++ cliente.py
@@ -0,0 +1,18 @@
+
+class Cliente:
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+cliente = Cliente("Jurema")
+
+#cliente.nome = ("asas")
+
+print(cliente.nome)
--- conta.py
+++ conta.py
@@ -2,19 +2,48 @@
 class Conta :
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(" {} possui {} R$ em sua conta.".format(self.titular, self.saldo))
+        print(" {} possui {} R$ em sua conta.".format(self.__titular, self.__saldo))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel = (self.__saldo + self.__limite)
+        return valor_a_sacar <= valor_disponivel
 
     def sacar(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O saque de R${} está acima do limite".format(valor))
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
 
 
 
@@ -22,5 +51,14 @@ conta = Conta(15700,"Pablito", 5000.00, 10000.00)
 conta2 = Conta(19700,"Jurema", 9000.00, 10000.00)
 conta3 = Conta(18700,"Lisa", 974.00, 10000.00)
 
-conta3.sacar(200)
+conta.extrato()
 conta3.extrato()
+conta3.transferir(200, conta)
+conta.extrato()
+conta3.extrato()
+
+print(conta.saldo)
+
+conta3.sacar(500000)
+
+print(Conta.codigo_banco())
